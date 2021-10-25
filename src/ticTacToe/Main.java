f9973e6cdaca9e852ccea90fd36b77b69211dc4e
nerai <nerai@LAPTOP-5Q7PRKVR.lan>
2021-10-25 21:00:52 +0200

    test

--- src/ticTacToe/Main.java
+++ src/ticTacToe/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("hej allesammans :) ");
-		System.out.println("Vill dela")
+		System.out.println("Ser ni detta Andom");
 	}
 
 }
